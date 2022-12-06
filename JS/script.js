e8d9004799f55a4c5dee96e6a4158190881c6150
Ebenezer Odame Aniagyei <99404199+MrEben@users.noreply.github.com>
2022-12-06 01:49:25 +0000

    added  new page

--- JS/script.js
+++ JS/script.js
@@ -9,3 +9,28 @@ const navSlide = () => {
   });
 };
 navSlide();
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const updateDate = () => {
+  const todaysDate = document.querySelector('.todays-date');
+  const todaysTime = document.querySelector('.todays-time');
+  const today = new Date();
+  console.log(today.getTime());
+  todaysDate.innerHTML = `<h4>${
+    months[today.getMonth()]
+  } ${today.getDate()},${today.getFullYear()}</h4>`;
+  todaysTime.innerHTML = `<h4>${today.getHours()}:${today.getMinutes()} GMT</h4>`;
+};
+updateDate();
